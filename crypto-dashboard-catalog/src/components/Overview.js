d4c900e06d47f5bf9a8497fb528e164db3b02a74
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-05 21:16:34 +0530

    mocked pricing

--- crypto-dashboard-catalog/src/components/Overview.js
+++ crypto-dashboard-catalog/src/components/Overview.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Card from './Card'
 
-const Overview = () => {
+const Overview = ({price, change, changePercent,symbol}) => {
   return (
-    <div>Overview</div>
+    <Card>
+        <div className="w-full h-full flex-col justify-start items-center">
+            <div className="">
+            <span className="text-3xl xl:text-6xl 2xl:text-8xl">${price}
+            </span>
+            <span className="absolute text-neutral-400 text-sm">{symbol}</span>
+            
+            </div>
+        <span className={`text-sm xl:text-base 2xl:text-base ${change>0 ? "text-lime-500" : "text-red-500"}`}>+ {change} <span>({changePercent}%)</span>
+        </span>
+        </div>
+    </Card>
   )
 }
 

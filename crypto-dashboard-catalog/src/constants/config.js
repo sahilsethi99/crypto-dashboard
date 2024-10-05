156d5b5fb725233d83643a7d37981bc93606d661
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-05 16:40:46 +0530

    initial commit

---
+++ crypto-dashboard-catalog/src/constants/config.js
@@ -0,0 +1,10 @@
+export const chartConfig = {
+    "1D" : {days: 1, weeks: 0, months: 0, years: 0},
+    "3D" : {days: 3, weeks: 0, months: 0, years: 0},
+    "1W" : {days: 0, weeks: 1, months: 0, years: 0},
+    "1M" : {days: 0, weeks: 0, months: 1, years: 0},
+    "6M" : {days: 0, weeks: 0, months: 6, years: 0},    
+    "1Y" : {days: 0, weeks: 0, months: 0, years: 1},
+    
+    
+}

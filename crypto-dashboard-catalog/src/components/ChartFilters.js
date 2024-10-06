3ed1b25cb7f84662e97d105678c8c590185ee3b0
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-07 02:09:53 +0530

    adding darkMode

--- crypto-dashboard-catalog/src/components/ChartFilters.js
+++ crypto-dashboard-catalog/src/components/ChartFilters.js
@@ -9,18 +9,19 @@ const ChartFilters = () => {
     const {filter,setFilter} = useContext(FilterContext);
   return (
     <>
-    <div className="flex ">
-    
+    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-row md:flex-row my-2 md:my-0">
 
-    <div className="flex mr-10">
+    <div className="flex mr-10  md:flex">
         <img src="/expand-gray-icon.png" alt="Expand Icon" className="w-6 h-6 p-1 mr-1 " />
         <div className="text-[#8B8B8B]">Fullscreen</div>
         </div>
-    <div className="flex mr-20">
+    <div className="flex mr-20  md:flex">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#8B8B8B" className="size-6 mr-2">
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
         <div className="text-[#8B8B8B]">Compare</div>      
+    </div>
     </div>
     <ul className="flex">
         {Object.keys(chartConfig).map((item) => {
